692885d7665a4c8912b71c9f6eb41fbb770592d6
karavanjo <jownkiv@gmail.com>
2013-06-04 11:51:01 +0300

    Min js files were updated

--- openpolice/static/build/op.js
+++ openpolice/static/build/op.js
@@ -628,18 +628,23 @@ var OP = {};
         },
 
 
-        searchInputHandler: [],
+        timeout: null,
         manageSearchInputs: function (event, searchValue) {
-            var context = this;
-            this.searchInputHandler.push([event, searchValue]);
-            setTimeout(function () {
-                var countInputHandlers = context.searchInputHandler.length;
-                if (countInputHandlers > 1) {
-                    context.searchInputHandler.splice(0, 1);
-                } else if (countInputHandlers === 1) {
-                    context.searchKeyHandler(context.searchInputHandler[0][0], context.searchInputHandler[0][1]);
-                    context.searchInputHandler.splice(0, 1);
-                }
+            var context = this,
+                keycode = event.keyCode || event.which;
+
+            if (keycode === 13) {
+                this.search(searchValue, 'searchByEnter');
+            }
+
+            if (this.timeout) {
+                clearTimeout(this.timeout);
+                this.timeout = null;
+            }
+            this.timeout = setTimeout(function () {
+                clearTimeout(context.timeout);
+                context.timeout = null;
+                context.searchKeyHandler(event, searchValue);
             }, 1000);
         },
 
@@ -647,7 +652,7 @@ var OP = {};
         isSearchResultsExist: false,
         searchKeyHandler: function (event, searchValue) {
             if (this.validateSearch(searchValue)) {
-                this.search(searchValue);
+                this.search(searchValue, 'searchByChar');
             } else if (this.isSearchResultsExist) {
                 this.clearSearchResults();
             }
@@ -668,30 +673,46 @@ var OP = {};
 
 
         countSearchResultsVisible: 7,
-        search: function (address) {
+        search: function (address, handler) {
             var context = this,
-                matches,
-                matchesCount = 0;
+                matches;
             address = this.prepareSearch(address);
             OP.view.$searchResults.empty().addClass('loader');
             OP.view.$document.trigger('/op/osm/geocoder/direct', [address, function (result) {
-                matches = result.matches;
-                if (matches) {
-                    matchesCount = result.matches.length;
-                    if (matchesCount > context.countSearchResultsVisible) {
-                        matches = matches.slice(0, context.countSearchResultsVisible);
-                    }
-                }
                 OP.view.$searchResults.removeClass('loader');
-                context.isSearchResultsExist = true;
-                OP.view.$searchResults.empty().append(OP.templates['search-item']({
-                    matches: matches
-                }));
-
-                context.bindEventsAfterSearch(matchesCount);
+                matches = result.matches;
+                context[handler](matches);
             }]);
         },
 
+
+        searchByChar: function (matches) {
+            var matchesCount = 0;
+            if (matches) {
+                matchesCount = matches.length;
+                if (matchesCount > this.countSearchResultsVisible) {
+                    matches = matches.slice(0, this.countSearchResultsVisible);
+                }
+            }
+            this.buildSearchResults(matches);
+            this.bindEventsAfterSearch(matchesCount);
+        },
+
+
+        searchByEnter: function (matches) {
+            if (matches) {
+                this.goToSearchedAddress(matches[0].lat, matches[0].lon);
+            } else {
+                this.buildSearchResults(null);
+            }
+        },
+
+
+        buildSearchResults: function (matches) {
+            OP.view.$searchResults.empty().append(OP.templates['search-item']({
+                matches: matches
+            }));
+        },
 
         prepareSearch: function (address) {
             return 'Москва, ' + address;
@@ -704,12 +725,23 @@ var OP = {};
             if (matchesCount > 0) {
                 OP.view.$searchResults.find('li.address').off('click').on('click', function (e) {
                     var $this = $(this);
-                    context.clearSearchResults();
-                    OP.view.$document.trigger('/op/map/setview', [$this.data('lat'), $this.data('lng')]);
+                    context.goToSearchedAddress($this.data('lat'), $this.data('lng'));
                 });
             } else {
-                OP.view.$searchResults.find('ul').delay(1000).fadeOut(1000);
+                this.showNotFoundResults();
             }
+        },
+
+
+        goToSearchedAddress: function (lat, lng) {
+            this.clearSearchResults();
+            OP.viewmodel.map.closePopup();
+            OP.view.$document.trigger('/op/map/setview', [lat, lng]);
+        },
+
+
+        showNotFoundResults: function () {
+            OP.view.$searchResults.find('ul').delay(1000).fadeOut(1000);
         },
 
 
