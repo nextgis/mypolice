f96a1becaf07b0fd38fcd92a71ec0fec97a4aff3
karavanjo <jownkiv@gmail.com>
2014-03-16 17:13:46 +0300

    changed folder to policeman photo

--- openpolice/houses.py
+++ openpolice/houses.py
@@ -34,7 +34,7 @@ def get_houses(context, request):
 
         policeman = house.policeman
         if not policeman.id in policemen:
-	    photo_url_parsed = 'http://112.ru/publish/00/00/uum/45000000000/45296000000/45296597000/img11.jpg'.split('/')	    
+	    photo_url_parsed = policeman.photo_url.split('/')	    
 	    photo_name = photo_url_parsed[-2] + photo_url_parsed[-1].replace('img', '_file')
             policemen[policeman.id] = {
                 'id': policeman.id,
@@ -43,7 +43,7 @@ def get_houses(context, request):
                 'rank': policeman.rank,
                 'phone': policeman.phone,
                 'url': policeman.url,
-                'photo_url': request.route_url('home') + 'static/images/photos/' + photo_name,
+                'photo_url': request.route_url('home') + 'static/images/' + photo_name,
                 'color': policeman.color
             }
 
